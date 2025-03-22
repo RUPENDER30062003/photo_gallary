e6a816ccf44fccca18fe38bf985b307b13cc81fd
RUPENDER <rs9154040@gmail.com>
2025-03-22 20:32:13 +0530

    Final push

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,45 @@
 // })
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
+
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     historyApiFallback: true,
+//   },
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    historyApiFallback: true,
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
   },
-})
+  plugins: [react()],
+});
